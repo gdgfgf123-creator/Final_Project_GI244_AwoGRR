5d562e662b2a8e31f89de5b0e5f0084b1e0689f4
Itt <gdgfgf123@gmail.com>
2026-05-04 00:52:47 +0700

    UI save

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public GameObject mainMenuPanel;
+    public GameObject levelSelectPanel;
     public GameObject gameOverUI;
 
     void Awake()
@@ -14,9 +16,31 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        mainMenuPanel.SetActive(true);
+        levelSelectPanel.SetActive(false);
+
         if (gameOverUI != null)
             gameOverUI.SetActive(false);
     }
+
+    public void StartGame()
+    {
+        mainMenuPanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void LoadLevel1()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Lv1");
+    }
+
+    public void LoadLevel2()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Lv2");
+    }
+
     public void GameOver()
     {
         if (gameOverUI != null)
@@ -24,11 +48,19 @@ public class GameManager : MonoBehaviour
 
         Time.timeScale = 0f;
     }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
 
+        Application.Quit(); 
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
     public void RestartGame()
     {
         Time.timeScale = 1f;
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
